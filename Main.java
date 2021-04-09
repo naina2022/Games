78d5db6180445bbb47d22ee79d74f40eb3638fc9
naina2022 <60146073+naina2022@users.noreply.github.com>
2021-04-09 09:22:58 -0400

    Update Main.java
    Added series of if-statements and Scanner class to get user input for what game they want to play.

--- Main.java
+++ Main.java
@@ -7,15 +7,78 @@
  * in a series of if-statements prompting the user which
  * game they want to play 
  *
+ * TO DO: Word Game with countries, states, etc.  
  */
 
 import java.util.Scanner; 
 class Main {
-  public static void main(String[] args) {
-    /*MastermindMain m1 = new MastermindMain(); 
-    m1.play(); */
 
-    BattleshipMain m2 = new BattleshipMain(); 
-    m2.play(); 
+  //plays Mastermind 
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in); 
+    //prompt user which game they want to play 
+    System.out.println(); 
+    System.out.println("Welcome! Here are all of the game options: "); 
+    System.out.println("Battleship"); 
+    System.out.println("Mastermind"); 
+    System.out.println("TicTacToe"); 
+    System.out.println("Connect Four"); 
+    System.out.println(); 
+
+    boolean check = false; 
+    int answer; 
+
+    //prompts the user for an input, and keeps asking if answer is not valid 
+    do
+    {
+      System.out.print("Which game do you want to play? Battleship (1), Mastermind (2), TicTacToe (3), or Connect Four (4): "); 
+      answer = input.nextInt();
+      if(answer == 1 || answer == 2|| answer == 3 || answer == 4)
+      {
+        check = true; 
+      }
+    } 
+    while(!check); 
+
+    if(answer == 1)
+    {
+      //clears the console 
+      System.out.print("\033[H\033[2J");  
+      System.out.flush();
+      System.out.println();
+
+      BattleshipMain m2 = new BattleshipMain(); 
+      m2.play(); 
+    }
+    else if(answer == 2)
+    {
+      //clears the console 
+      System.out.print("\033[H\033[2J");  
+      System.out.flush();
+      System.out.println();
+
+      MastermindMain m1 = new MastermindMain(); 
+      m1.play(); 
+    }
+    else if(answer == 3)
+    {
+      //clears the console 
+      System.out.print("\033[H\033[2J");  
+      System.out.flush();
+      System.out.println();
+
+      TicTacToeMain m3 = new TicTacToeMain(); 
+      m3.play(); 
+    }
+    else if(answer == 4)
+    {
+      //clears the console 
+      System.out.print("\033[H\033[2J");  
+      System.out.flush();
+      System.out.println();
+
+      ConnectFourMain m4 = new ConnectFourMain(); 
+      m4.play(); 
+    }
   }
 }
