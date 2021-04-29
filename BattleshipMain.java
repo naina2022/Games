286ff01e407ec3cd09fc309af8937f680eed8eac
naina2022 <60146073+naina2022@users.noreply.github.com>
2021-04-29 08:26:33 -0400

    Update BattleshipMain.java

--- BattleshipMain.java
+++ BattleshipMain.java
@@ -1,12 +1,13 @@
 /* Naina Purushothaman 
- * 9/29/20 
- * Battleship 
- * Main method, calls methods from Board and Play classes to allow the user 
+ * 4/7/21 
+ * BattleshipMain.java  
+ * Main method, calls methods from BattleshipBoard and BattleshipPlay classes to allow the user 
  * to play the game   
  */
 
 public class BattleshipMain 
 {
+  //the method which will play the game
 	public void play() 
 	{
 		BattleshipBoard b1 = new BattleshipBoard(); //a constructor 
@@ -19,34 +20,42 @@ public class BattleshipMain
  
 		//statically sets the ships on the play board for the user to guess 
 		b1.setShips(play1); 
-	
-		//b1.printArray(play1); 
 
 		System.out.println(); 
 
-		//prints the board for the user  
+		//prints the board for the user (guesser)
 		b1.printArray(guess1); 
 
 		//plays the game 
 		BattleshipPlay g1 = new BattleshipPlay(); //a constructor 
+
+    //displays the rules of the game 
 		g1.showRules(); 
-		int check = 0; 
+
+		int check = 0;  //variable that will keep track of how many ships are sunk
 		do
 		{ 
-			int[] guesses = g1.guessUser();   
+			int[] guesses = g1.guessUser(); //gets the guess from the user   
 			System.out.println(); 
+
+      //checks if the guess is valid and hasn't been guessed before
 			g1.checkUser(guesses, guess1, play1); 
+
+      //checks if the user has sunk a ship 
 			check = g1.checkSink(guess1, check); 
+
+      //prints out the array for the user to see whether they have sunk a ship or missed 
 			b1.printArray(guess1);
 			System.out.println(); 
-		}
+		} //end do while 
 		while(check != 3);
 
+    //end of game message 
 		System.out.println("Congratulations! You won the game! Here is your final board: ");
 		System.out.println(); 
+
+    //prints the final array of guesses to show the guesser 
 		b1.printArray(guess1); 
  
 	}  //close main
-
- 
 } //close class 
