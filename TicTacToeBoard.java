7751c42a8e8e0bb2178a10ada2ca03785ee0bb9e
naina2022 <60146073+naina2022@users.noreply.github.com>
2021-04-29 08:31:22 -0400

    Update TicTacToeBoard.java

--- TicTacToeBoard.java
+++ TicTacToeBoard.java
@@ -4,8 +4,8 @@
  * TicTacToeBoard.java 
  * Creates the 3x3 board and has methods to access elements at 
  * specific locations and a getter method for the board 
- *
 */
+
 public class TicTacToeBoard
 {
   //instance variable for the 2D board array 
@@ -23,14 +23,14 @@ public class TicTacToeBoard
       for(int j = 0; j < 3; j++)
       {
         board[i][j] = "E"; 
-      }
-       
-    }
-  }
+      }//end inner for  
+    } //end outer for 
+  } //end TictacToeBoard() 
 
   //method to print the board 
   public void printBoard(String[][] board1)
   {
+    //prints board header 
     System.out.println("  0 1 2"); 
 		for(int x = 0; x < board1.length; x++)   //x is the row 
 		{
@@ -43,7 +43,7 @@ public class TicTacToeBoard
 			//this prints a new line after every row is printed 
 			System.out.println(); 
 		} //close x loop	 
-  }
+  } //end printBoard() 
 
   /*method to return the String at a certain position
     input the row and column number (starting at 1) 
@@ -51,11 +51,11 @@ public class TicTacToeBoard
   public String atBoard(int r, int c)
   {
     return board[r-1][c-1]; 
-  }
+  } //end atBoard() 
 
   //getBoard method 
   public String[][] getBoard()
   {
     return board; 
-  }
-}
+  } //end getBoard() 
+} //end class 
