812913804c362f9264ddb756b2432e0655d2cfba
naina2022 <60146073+naina2022@users.noreply.github.com>
2021-04-29 08:25:15 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,28 +1,28 @@
 /* 
- *
  * Naina Purushothaman 
  * Main.java 
  * 3/10/21
  * The class with the main method, calls all the games 
  * in a series of if-statements prompting the user which
  * game they want to play 
- *
- * TO DO: Word Game with countries, states, etc.  
  */
 
 import java.util.Scanner; 
-class Main {
+class Main 
+{
 
-  //plays Mastermind 
+  //prints out all the game options that the user can play 
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in); 
     //prompt user which game they want to play 
     System.out.println(); 
     System.out.println("Welcome! Here are all of the game options: "); 
-    System.out.println("Battleship"); 
-    System.out.println("Mastermind"); 
-    System.out.println("TicTacToe"); 
-    System.out.println("Connect Four"); 
+    System.out.println(); 
+    System.out.println("Battleship (1)"); 
+    System.out.println("Mastermind (2)"); 
+    System.out.println("TicTacToe (3)"); 
+    System.out.println("Connect Four (4)"); 
+    System.out.println("Twenty Questions (5)"); 
     System.out.println(); 
 
     boolean check = false; 
@@ -31,15 +31,16 @@ class Main {
     //prompts the user for an input, and keeps asking if answer is not valid 
     do
     {
-      System.out.print("Which game do you want to play? Battleship (1), Mastermind (2), TicTacToe (3), or Connect Four (4): "); 
+      System.out.print("Which game do you want to play. Enter a number: "); 
       answer = input.nextInt();
-      if(answer == 1 || answer == 2|| answer == 3 || answer == 4)
+      if(answer == 1 || answer == 2|| answer == 3 || answer == 4 || answer == 5)
       {
         check = true; 
-      }
-    } 
+      } //end if
+    } //end do while
     while(!check); 
 
+    //series of if-statements that checks what the user inputted and plays the corresponding game
     if(answer == 1)
     {
       //clears the console 
@@ -47,9 +48,12 @@ class Main {
       System.out.flush();
       System.out.println();
 
+      //creates an instance of the BattleshipMain class 
       BattleshipMain m2 = new BattleshipMain(); 
+
+      //calls the method in BattleshipMain called play() which plays the game
       m2.play(); 
-    }
+    } //end if
     else if(answer == 2)
     {
       //clears the console 
@@ -57,9 +61,12 @@ class Main {
       System.out.flush();
       System.out.println();
 
+      //creates an instance of the MastermindMain class 
       MastermindMain m1 = new MastermindMain(); 
+
+      //calls the method in MastermindMain called play() which plays the game
       m1.play(); 
-    }
+    } //end else if
     else if(answer == 3)
     {
       //clears the console 
@@ -67,9 +74,12 @@ class Main {
       System.out.flush();
       System.out.println();
 
+      //creates an instance of the TictacToeMain class 
       TicTacToeMain m3 = new TicTacToeMain(); 
+
+      //calls the method in TicTacToeMain called play() which plays the game 
       m3.play(); 
-    }
+    } //end else if
     else if(answer == 4)
     {
       //clears the console 
@@ -77,8 +87,24 @@ class Main {
       System.out.flush();
       System.out.println();
 
+      //creates an instance of the ConnectFourMain class 
       ConnectFourMain m4 = new ConnectFourMain(); 
+
+      //calls the method in ConnectFourMain called play() which plays the game
       m4.play(); 
-    }
-  }
-}
+    } //end else if
+    else if(answer == 5)
+    {
+      //clears the console 
+      System.out.print("\033[H\033[2J");  
+      System.out.flush();
+      System.out.println();
+
+      //creates an instance of the TwentyQuestionsMain class 
+      TwentyQuestionsMain m5 = new TwentyQuestionsMain(); 
+
+      //calls the method in TwentyQuestionsMain called play() which plays the game 
+      m5.play(); 
+    } //end else if
+  } //end main method 
+} //end class 
